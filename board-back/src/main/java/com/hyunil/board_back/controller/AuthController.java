b8b0afcb4500ab795b5ea092bfdd2c46d2fe04ce
hy0327 <hy_0327@naver.com>
2024-09-12 20:55:05 +0900

    로그인페이지 1

--- board-back/src/main/java/com/hyunil/board_back/controller/AuthController.java
+++ board-back/src/main/java/com/hyunil/board_back/controller/AuthController.java
@@ -1,19 +1,20 @@
 package com.hyunil.board_back.controller;
 
+import jakarta.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hyunil.board_back.dto.request.auth.SignInRequestDto;
 import com.hyunil.board_back.dto.request.auth.SignUpRequestDto;
 import com.hyunil.board_back.dto.response.auth.SignUpResponseDto;
+import com.hyunil.board_back.dto.response.auth.SignInResponseDto;
 import com.hyunil.board_back.service.AuthService;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
