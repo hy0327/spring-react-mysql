d69210de7418a907bed0ee4c571a4b18021ca587
hy0327 <hy_0327@naver.com>
2024-11-21 20:39:00 +0900

    유저페이지 구현

--- board-front/src/apis/response/board/index.ts
+++ board-front/src/apis/response/board/index.ts
@@ -6,6 +6,7 @@ import PutFavoriteResponseDto from "./put-favorite-response.dto";
 import DeleteBoardResponseDto from "./delete-board-response.dto";
 import GetCommentListResponseDto from "./get-comment-list-response.dto";
 import GetFavoriteListResponseDto from "./get-favorite-list.response.dto";
+import GetUserBoardListResponseDto from "./get-user-board-list.response.dto";
 import GetTop3BoardListResponseDto from "./get-top-3-board-list.response.dto";
 import IncreaseViewCountResponseDto from "./increase-view-count.response.dto";
 import GetLatestBoardListResponseDto from "./get-latest-board-list.response.dto";
@@ -21,6 +22,7 @@ export type {
     GetCommentListResponseDto,
     GetFavoriteListResponseDto,
     GetTop3BoardListResponseDto,
+    GetUserBoardListResponseDto,
     IncreaseViewCountResponseDto,
     GetLatestBoardListResponseDto,
     GetSearchBoardListResponseDto
